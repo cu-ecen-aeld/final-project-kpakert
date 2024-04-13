7a0fd06abce7174ed092a24a6977eb43ee1914a1
kpakert <kpakert@gmail.com>
2024-04-13 11:19:38 -0500

    removing specific distro definition and v4l as I don't need them

--- build.sh
+++ build.sh
@@ -20,7 +20,7 @@ MEMORY="GPU_MEM = \"16\""
 #Add wifi support
 DISTRO_F="DISTRO_FEATURES:append = \"wifi\""
 #add firmware support 
-IMAGE_ADD="IMAGE_INSTALL:append = \"linux-firmware-rpidistro-bcm43430 v4l-utils python3 ntp wpa-supplicant\""
+IMAGE_ADD="IMAGE_INSTALL:append = \"python3 ntp wpa-supplicant\""
 
 
 #Licence
